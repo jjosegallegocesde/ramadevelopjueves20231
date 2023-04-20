cfc382916c6b6619e6976595b781c96269a23f1a
Ruddy Rodriguez <ruddy.rodriguez.r@hotmail.com>
2023-04-20 08:49:12 -0500

    se crea archivo main...sufran

--- Maracuya.py
+++ Maracuya.py
@@ -1,10 +1,38 @@
 class Maracuya:
 
     def __init__(self):
-        self.nombre=None
-        self.cantidad=None
-        self.estado=None
-        self.precio=None
+        self.__nombre=None
+        self.__cantidad=None
+        self.__estado=None
+        self.__precio=None
+     #GETTERS(OBTENER INFO/MOSTRANDO)
+    @property
+    def nombre(self):
+        return self.__nombre
+    @property
+    def cantidad(self):
+        return self.__cantidad
+    @property
+    def estado(self):
+        return self.__estado
+    @property
+    def precio(self):
+        return self.__precio
+    #SETTERS(INGRESAR INFORMACION/GUARDANDO)
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre=dato
+    @cantidad.setter
+    def cantidad(self,dato):
+        self.__cantidad=dato
+    @estado.setter
+    def estado(self,dato):
+        self.__estado=dato
+    @precio.setter
+    def precio(self,dato):
+        self.__precio=dato
+
+
 
     def describirFruta(self):
         print(f'mi fruta Maracuya es deliciosa y sirve para darle acidez al salpicon')
---
+++ main.py
@@ -0,0 +1,13 @@
+from Maracuya import Maracuya
+
+objecto=Maracuya()
+
+#asignar valor al atributo nombre de maracuya
+objecto.nombre="Maracuya purpura"
+print(objecto.nombre)
+objecto.cantidad="1kilo"
+print(objecto.cantidad)
+objecto.estado="Madura"
+print(objecto.estado)
+objecto.precio="3000"
+print(objecto.precio)
